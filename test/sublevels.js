8ff6271d2a5d7f6c3a29b13801df14b4e576a5fc
Riceball LEE <snowyu.lee@gmail.com>
2014-11-01 10:31:50 +0800

    + [test] the hooks and sublevels free on close.

--- test/sublevels.js
+++ test/sublevels.js
@@ -16,8 +16,8 @@ test('subsections', function (t) {
   t.strictEqual(base.sublevel('foo'), foo)
   t.strictEqual(base.sublevel('bar'), bar)
 
-  console.log('prefix:', foo.path())
-  console.log('prefix:', bar.path())
+  console.log('path:', foo.path())
+  console.log('path:', bar.path())
 
   var fooBlerg = foo.sublevel('blerg')
   t.deepEqual(foo.sublevels, {'blerg': fooBlerg})
@@ -27,6 +27,36 @@ test('subsections', function (t) {
   t.end()
 })
 
+test('sublevels-hooks-free', function (t) {
+  var bar = base.sublevel('newBar')
+  t.deepEqual(bar.sublevels, {})
+  var barLess = bar.sublevel('Less')
+  var barMore = bar.sublevel('more')
+
+  t.deepEqual(bar.sublevels, {'Less': barLess, 'more': barMore})
+
+  t.strictEqual(bar.sublevel('more'), barMore)
+  t.strictEqual(bar.sublevel('Less'), barLess)
+
+  bar.pre(function(){})
+  bar.post(function(){})
+  barLess.pre(function(){})
+  barLess.post(function(){})
+  barMore.pre(function(){})
+  barMore.post(function(){})
+
+  t.strictEqual(bar.unhooks.length, 2)
+  t.strictEqual(barLess.unhooks.length, 2)
+  t.strictEqual(barMore.unhooks.length, 2)
+  base.close()
+  t.strictEqual(bar.unhooks.length, 0)
+  t.strictEqual(barLess.unhooks.length, 0)
+  t.strictEqual(barMore.unhooks.length, 0)
+  t.deepEqual(bar.sublevels, {})
+  t.deepEqual(barLess.sublevels, {})
+  t.deepEqual(barMore.sublevels, {})
+  t.end()
+})
 
 
 
