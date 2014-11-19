ef95aab80de38b6077b4465c5e7cf49944046677
Riceball LEE <snowyu.lee@gmail.com>
2014-11-19 13:26:42 +0800

    * [bug] some inherits fixed + isInheritedFrom, inheritsObject function + unit test for util

--- util.js
+++ util.js
@@ -53,20 +53,37 @@ var util = module.exports = {
      */
     inherits: function(ctor, superCtor) {
       var v  = ctor.super_;
-      ctor.super_ = superCtor;
-      ctor.__super__ = superCtor.prototype; //for coffeeScirpt super keyword.
-      ctor.prototype = util.newPrototype(superCtor, ctor);
-      if (v != null) {
-        inherits(superCtor, v);
-        /*
-        if (superCtor.super_ == null) {
-          superCtor.super_ = v;
-          superCtor.__super__ = v.prototype;
-          superCtor.prototype = util.newPrototype(v, superCtor);
-        } else {
-          throw new Error("superCtor has already super_ exists!");
-        } //*/
+      var result = false;
+      if (!util.isInheritedFrom(ctor, superCtor)) {
+        ctor.super_ = superCtor;
+        ctor.__super__ = superCtor.prototype; //for coffeeScirpt super keyword.
+        ctor.prototype = util.newPrototype(superCtor, ctor);
+        //result = util.inherits(superCtor, v);
+        while (v != null) {
+          ctor = superCtor;
+          superCtor = v;
+          ctor.super_ = superCtor;
+          ctor.__super__ = superCtor.prototype; //for coffeeScirpt super keyword.
+          ctor.prototype = util.newPrototype(superCtor, ctor);
+          v = ctor.super_;
+        }
+        result = true;
       }
+      return result;
+    },
+    isInheritedFrom: function(ctor, superCtor) {
+      var result  = ctor.super_ === superCtor;
+      var checks = [];
+      var lastCtor = ctor;
+      checks.push(ctor);
+      while (!result && ((ctor = ctor.super_) != null)) {
+        if (checks.indexOf(ctor) >= 0) {
+          throw Error("Circular inherits found!");
+        }
+        checks.push(ctor);
+        result = ctor.super_ === superCtor;
+      }
+      return result;
     },
     newPrototype: function(aClass, aConstructor) {
       //Object.create(prototype) only for ES5
@@ -75,7 +92,7 @@ var util = module.exports = {
       //  var Object = function() { this.constructor = aConstructor; };
       //  Object.prototype = aClass.prototype;
       //  return new Object();
-      return Object.create(aClass.prototype, {
+      var result = Object.create(aClass.prototype, {
         constructor: {
           value: aConstructor,
           enumerable: false,
@@ -83,5 +100,30 @@ var util = module.exports = {
           configurable: true
         }
       });
+      util._extend(result, aConstructor.prototype);
+      return result;
+    },
+    _extend: function(origin, add) {
+      // Don't do anything if add isn't an object
+      if (!add || !util.isObject(add)) return origin;
+
+      var keys = Object.keys(add);
+      var i = keys.length;
+      while (i--) {
+        origin[keys[i]] = add[keys[i]];
+      }
+      return origin;
+    },
+    //make sure the aClass.prototype hook to the aObject instance.
+    inheritsObject: function(aObject, aClass) {
+      var vOldProto = aObject.__proto__;
+      var result = false;
+      if ( vOldProto !== aClass.prototype) {
+        if (util.inherits(aClass, vOldProto.constructor)) {
+          aObject.__proto__ = aClass.prototype;
+          result = true;
+        }
+      }
+      return result;
     }
 }
